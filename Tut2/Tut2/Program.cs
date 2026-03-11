0d12a4613f213a119cef2c3368c576ab75b02cda
Borys Czajkowski <s33205@pjwstk.edu.pl>
2026-03-11 09:14:39 +0100

    Dodano klase Student

--- Tut2/Tut2/Program.cs
+++ Tut2/Tut2/Program.cs
@@ -1,3 +1,12 @@
-﻿Console.WriteLine("Wpisz swoje imie");
-string napis = Console.ReadLine();;
-Console.WriteLine(napis);
+﻿namespace Tut2;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Wpisz swoje imie");
+        string napis = Console.ReadLine();
+        Console.WriteLine(napis);
+        Student student = new Student(25, "Jarek", 5.0f);
+    }
+}
---
+++ Tut2/Tut2/Student.cs
@@ -0,0 +1,23 @@
+﻿namespace Tut2;
+
+public class Student
+{
+    private int wiek { get; set; }
+    private string imie { get; set; }
+    private float sredniaOcen { get; set; }
+
+    public Student(int wiek, string imie, float sredniaOcen)
+    {
+        this.wiek = wiek;
+        this.imie = imie;
+        this.sredniaOcen = sredniaOcen;
+    }
+    
+    public void printStud()
+    {
+        Console.WriteLine("Moje imie to: " +
+                          imie + ", mam " + wiek + " lat"
+                          + ", a moja srednia ocen to " + sredniaOcen);
+    }
+    
+}
