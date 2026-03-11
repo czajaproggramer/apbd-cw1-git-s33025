6a92e1afef11467d52434d997a4510cbdb93c2f9
Borys Czajkowski <s33205@pjwstk.edu.pl>
2026-03-11 09:42:10 +0100

    Zmieniono nazwe zmiennej

--- Tut2/Tut2/Program.cs
+++ Tut2/Tut2/Program.cs
@@ -5,7 +5,7 @@ public class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Wpisz swoje imie");
-        string imie = Console.ReadLine();
+        string nazwa = Console.ReadLine();
         Console.WriteLine("Wpisz swoj wiek");
         int wiek = Int32.Parse(Console.ReadLine());
         List<int> oceny = new List<int>();
@@ -14,7 +14,7 @@ public class Program
         oceny.Add(3);
         oceny.Add(4);
         oceny.Add(7);
-        Student student = new Student(wiek, imie, oceny);
+        Student student = new Student(wiek, nazwa, oceny);
         student.printStud();
     }
 
